fc9217c6250800fa6887ec052c31e8bce6ad768a
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-07-26 00:30:40 +0900

    追加

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>ようこそ</h1>
+            <p>ReactとTypeScriptを使用して簡単なWebページを作って見ました!</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Link.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Link: React.FC = () => {
+    return (
+        <div>
+            <h2>SNSとかGitHubのリンク</h2>
+        </div>
+    );
+};
+
+export default Link;
